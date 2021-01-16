2297d2fabc4e7ebc03a9c8ce081bc14302b4de5f
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-16 12:35:41 +0700

    #06 Event v-on:click with direct data(global variable) updates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ new Vue({
   el: "#vue-app",
   data: {
     name: "Wildan",
+    age: 25,
     job: "help people get their job",
     website: "http://wildank.blogspot.com",
     websiteTag: '<a href="http://wildank.blogspot.com">Wildank Website',
