c1c64fcf12ae021a8c7373b0c17de5593d9ff6d1
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-17 10:47:37 +0700

    23 conditional v-if

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 new Vue({
   el: "#vue-app",
   data: {
-    available: false,
-    nearby: false,
+    error: false,
+    success: false,
   },
   methods: {},
   computed: {
-    compClasses: function () {
-      return {
-        available: this.available,
-        nearby: this.nearby,
-      };
-    },
     /*    addToA: function () {
       console.log("addToA");
       return this.age + this.a;
