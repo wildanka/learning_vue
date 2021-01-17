384a46a73c77a841d426fc5008ed4e93ef59a88f
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-17 10:30:06 +0700

    20 dynamic css solution #1

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Wildan",
-    age: 25,
-    a: 0,
-    b: 0,
-    job: "help people get their job",
-    website: "http://wildank.blogspot.com",
-    websiteTag: '<a href="http://wildank.blogspot.com">Wildank Website',
+    available: true,
+    nearby: false,
   },
-  methods: {
-    /* addToA: function () {
+  methods: {},
+  computed: {
+    /*    addToA: function () {
       console.log("addToA");
       return this.age + this.a;
     },
@@ -18,15 +14,5 @@ new Vue({
       console.log("addToB");
       return this.age + this.b;
     }, */
-  },
-  computed: {
-    addToA: function () {
-      console.log("addToA");
-      return this.age + this.a;
-    },
-    addToB: function () {
-      console.log("addToB");
-      return this.age + this.b;
-    },
   },
 });
