3103b0a22224ff819526962d65cf2b1ec9900b11
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-17 15:35:25 +0700

    37 refs : getting dom access using refs

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ new Vue({
   methods: {
     readRefs: function () {
       //so refs do catch the dom of the 'refs'-es element
-      console.log(this.$refs.input.value);
+      console.log(this.$refs);
+      console.log(this.$refs.test.innerText);
       this.output = this.$refs.input.value;
     },
   },
