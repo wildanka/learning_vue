eed78dd016358d603fa6a5df017fcce7a39fb31d
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-17 12:38:16 +0700

    28 Multiple Vue Instances example

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-new Vue({
-  el: "#vue-app",
+var one = new Vue({
+  el: "#vue-app-one",
   data: {
-    health: 100,
-    ended: false,
+    title: "Vue App One",
   },
   methods: {
-    punch: function () {
-      this.health -= 10;
-      if (this.health <= 0) {
-        this.ended = true;
-      }
-    },
-    restart: function () {
-      this.health = 100;
-      this.ended = false;
+    changeTitle: function () {
+      two.title = "This is One hacked Two";
     },
   },
   computed: {
-    // ended: function () {
-    //   return this.ended;
-    // },
+    greet: function () {
+      return "Hello from app one";
+    },
   },
 });
+
+var two = new Vue({
+  el: "#vue-app-two",
+  data: {
+    title: "Vue App Two",
+  },
+  methods: {
+    changeTitle: function () {
+      one.title = "Title Changed";
+    },
+  },
+  computed: {
+    greet: function () {
+      return "Yo masbroo, disini app two";
+    },
+  },
+});
+
+two.title = "Changed from outside";
+
+/**
+ * if one day you found yourself having so many Vue Instances and there is to much communication between them
+ * if you constantly intracting with separate instances like this,
+ * maybe you should consider to restructure the application, you might want to bundle everything together in one Vue instances
+ * where it's much more natural to communicate between data.
+ * than having separate instances communicating with each other all over the show
+ */
