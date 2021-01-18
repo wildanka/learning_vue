7c1380de60a88a0bee6fbaabb7227e01c370ee6c
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-19 00:30:06 +0700

    46 Nesting Components Example : Solution step 3 (add Ninjas components) finished

--- learn_vue_cli/vue-with-cli/src/App.vue
+++ learn_vue_cli/vue-with-cli/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <app-header></app-header>
+    <ninjas></ninjas>
     <app-footer></app-footer>
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 import Header from './components/Headers.vue' //import components
 import Footer from './components/Footer.vue' //import components
+import Ninjas from './components/Ninjas.vue' //import components
 
 export default {
   components: { // add components
     'app-header': Header, //why not just use 'header' and not 'app-header'? because header is an html tag (remember <header>?) 
-    'app-footer': Footer //why not just use 'header' and not 'app-header'? because header is an html tag (remember <header>?) 
+    'app-footer': Footer, //why not just use 'header' and not 'app-header'? because header is an html tag (remember <header>?) 
+    'ninjas': Ninjas //why not just use 'header' and not 'app-header'? because header is an html tag (remember <header>?) 
   },
   data () {
     return {
---
+++ learn_vue_cli/vue-with-cli/src/components/Ninjas.vue
@@ -0,0 +1,54 @@
+<!-- create a Ninjas component-->
+<template>
+  <div id="ninjas">
+      <ul>
+          <li v-for="(ninja) in ninjas" v-on:click="ninja.show = !ninja.show" v-bind:key="ninja.name">
+              <h2>{{ninja.name}}</h2>
+              <h3 v-show="ninja.show">{{ninja.specialty}}</h3>
+          </li>
+      </ul>
+
+  </div>
+</template>
+<script>
+
+
+export default {
+  data () {
+    return {
+        ninjas: [
+            {name: 'Ryu', specialty:'Vue Components', show: false},
+            {name: 'Crystal', specialty:'HTML Wizard', show: false},
+            {name: 'Hiroshi', specialty:'Click Events', show: false},
+            {name: 'Takagi', specialty:'Conditionals', show: false},
+            {name: 'Wataru', specialty:'Webpack', show: false},
+            {name: 'Sato', specialty:'Data Diggin', show: false},
+        ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+ninjas{
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+}
+li{
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
+}
+</style>
