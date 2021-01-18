21eefe9bcf86d39c514c62fba70a8c6623b39c86
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-18 23:34:45 +0700

    41 Nesting Components (Locally)

--- learn_vue_cli/vue-with-cli/src/App.vue
+++ learn_vue_cli/vue-with-cli/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <!-- call the ninjas component -->
     <ninjas></ninjas> 
+    <!-- call the ninjas component -->
   </div>
   <!-- in Vue template you can only have one <div> root element, so remember! no other root div allowed! -->
 </template>
 
 <script>
-// import Ninjas from './Ninjas.vue'
+import Ninjas from './Ninjas.vue'
 export default {
-  // components: { Ninjas }, // register the 'Ninjas' component that we got from the main.js (since App.vue is registered on main.js too)
+  components: { Ninjas }, // register the 'Ninjas' component that we got from the main.js (since App.vue is registered on main.js too)
   data () {
     return {
       title: 'Ninja App!'
--- learn_vue_cli/vue-with-cli/src/main.js
+++ learn_vue_cli/vue-with-cli/src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Ninjas from "./Ninjas.vue"; // calling ninjas component from main app
+// import Ninjas from "./Ninjas.vue"; // calling ninjas component from main app
 
-Vue.component("ninjas", Ninjas); // register the component at the main app
+// Vue.component("ninjas", Ninjas); // register the component at the main app
 new Vue({
   el: "#app",
   render: h => h(App)
