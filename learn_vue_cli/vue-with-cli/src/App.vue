8a23464dce2cfd18548fe23a5a7e8c23c5fc58a0
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-19 00:03:36 +0700

    44 Nesting Components Example : Solution step 1 (add headers components)

--- learn_vue_cli/vue-with-cli/src/App.vue
+++ learn_vue_cli/vue-with-cli/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  
+  <app-header></app-header>
 </template>
 
 <script>
+import Header from './components/Headers.vue' //import components
 
 export default {
+  components: { // add components
+    'app-header': Header //why not just use 'header' and not 'app-header'? because header is an html tag (remember <header>?) 
+  },
   data () {
     return {
     }
---
+++ learn_vue_cli/vue-with-cli/src/components/Headers.vue
@@ -0,0 +1,29 @@
+<!-- create a headers component-->
+<template>
+  <header>
+      <h1>{{title}}</h1>
+  </header>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+        title: 'Vue Ninjas'
+    }
+  }
+}
+</script>
+
+<style>
+    header{
+        background: lightgreen;
+        padding: 10px;
+    }
+    h1{
+      color: #222;
+      text-align: center;
+    }
+
+</style>
