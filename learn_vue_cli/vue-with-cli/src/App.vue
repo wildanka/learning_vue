338507ee620bb483cd07a39c0df111b65d2c2d57
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-18 23:55:24 +0700

    43 Nesting Components Example : Starter Code

--- learn_vue_cli/vue-with-cli/src/App.vue
+++ learn_vue_cli/vue-with-cli/src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div>
-    <h1>{{title}}</h1>
-    <ninjas></ninjas> 
-    <!-- call the ninjas component -->
-  </div>
-  <!-- in Vue template you can only have one <div> root element, so remember! no other root div allowed! -->
+  
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
 
 export default {
-  components: { Ninjas }, // register the 'Ninjas' component that we got from the main.js (since App.vue is registered on main.js too)
   data () {
     return {
-      title: 'Ninja App!'
     }
   }
 }
 </script>
 
-<!-- this will make the h1 element colored purple-->
-<style scoped>
-h1{
-  color:purple
-}
+<style>
 </style>
