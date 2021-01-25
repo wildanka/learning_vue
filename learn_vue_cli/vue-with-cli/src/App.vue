acfb6eba0855722dfc303b81d8473dd3b5870e44
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-25 09:22:52 +0700

    61 slots : add some css styling and conclusion

--- learn_vue_cli/vue-with-cli/src/App.vue
+++ learn_vue_cli/vue-with-cli/src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <form-helper>
-      <h2 slot="title">{{ title }}</h2>
-      <p slot="text">I am the paragraph for the slot</p>
+      <div slot="form-header">
+        <h3>This is the title of the form</h3>
+        <p>Information about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required />
+        <input type="password" placeholder="password" required />
+      </div>
+      <div slot="form-controls">
+        <button @click="handleSubmit">Submit</button>
+      </div>
     </form-helper>
   </div>
 </template>
@@ -25,4 +34,8 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+  font-family: "Nunito SemiBold";
+}
 </style>
