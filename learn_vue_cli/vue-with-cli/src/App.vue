393ac1e315a4093b8beb70930a952f137c453ef5
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-27 23:17:30 +0700

    64 dynamic components : process 1

--- learn_vue_cli/vue-with-cli/src/App.vue
+++ learn_vue_cli/vue-with-cli/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <!-- first, this will work because we set component to display form-contact, 
-    next we'll try to make components dynamic-->
-    <component is="form-contact"></component>
+    <!-- now to make components dynamic, we got to do some binding to component using v-bind:is-->
+    <component v-bind:is="component"></component>
   </div>
 </template>
 
@@ -18,7 +17,8 @@ export default {
   },
   data() {
     return {
-      title: "I am the dynamic slot title",
+      //this component data will be binded to our component tag, this component data will contain the Vue Components we want to use
+      component: "form-contact",
     };
   },
   methods: {},
