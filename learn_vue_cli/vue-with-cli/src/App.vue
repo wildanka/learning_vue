3fd71fd6e3d4692278dfc40c8a186f95d28e3920
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-27 23:41:51 +0700

    65 dynamic components : process 2, make component show dynamic components

--- learn_vue_cli/vue-with-cli/src/App.vue
+++ learn_vue_cli/vue-with-cli/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <!-- now to make components dynamic, we got to do some binding to component using v-bind:is-->
-    <component v-bind:is="component"></component>
+    <component v-bind:is="(component = component)"></component>
+    <button v-on:click="component = 'form-login'">Go to Login</button>
+    <button v-on:click="component = 'form-contact'">Go to Contact</button>
   </div>
 </template>
 
