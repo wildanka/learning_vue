d1dbdec35c87aca7ced09a3bdbda681015b2eb84
Wildan Kurniadi <danprogramming10@gmail.com>
2021-01-28 00:37:06 +0700

    67 input binding in action (text) : starter code

--- learn_vue_cli/vue-with-cli/src/App.vue
+++ learn_vue_cli/vue-with-cli/src/App.vue
@@ -1,34 +1,19 @@
 <template>
   <div>
-    <!-- now to make components dynamic, we got to do some binding to component using v-bind:is-->
-    <!-- the keep-alive tag will notify Vue to make sure, 
-    the components inside <keep-alive> tag (which is component) data, won't be cleared/destroyed -->
-    <keep-alive
-      ><component v-bind:is="(component = component)"></component
-    ></keep-alive>
-    <!-- if we fill the form data in each form component, it will going to lose the data that we just input
-    and that's because everytime we change components, the components is kinda destroyed, 
-    and when we go back by clicking the same button, again it will create new instance of components -->
-    <button v-on:click="component = 'form-login'">Go to Login</button>
-    <button v-on:click="component = 'form-contact'">Go to Contact</button>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import FormContact from "./components/formContact.vue";
-import FormLogin from "./components/formLogin.vue";
+import addBlog from "./components/addBlog.vue";
 
 export default {
   components: {
     // add components
-    "form-login": FormLogin,
-    "form-contact": FormContact,
+    "add-blog": addBlog,
   },
   data() {
-    return {
-      //this component data will be binded to our component tag, this component data will contain the Vue Components we want to use
-      component: "form-contact",
-    };
+    return {};
   },
   methods: {},
 };
