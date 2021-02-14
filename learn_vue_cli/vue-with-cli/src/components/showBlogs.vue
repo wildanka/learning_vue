4f1f1901fc49a5e5aa50e7933349206fdd7ac04a
Wildan Kurniadi <danprogramming10@gmail.com>
2021-02-14 10:41:55 +0700

    76 custom directive : create our first old custom directives

--- learn_vue_cli/vue-with-cli/src/components/showBlogs.vue
+++ learn_vue_cli/vue-with-cli/src/components/showBlogs.vue
@@ -2,7 +2,11 @@
   <div id="show-blogs">
     <h1>All Blog Articles</h1>
     <div v-for="blog in blogs" class="single-blog" v-bind:key="blog.id">
-      <h2>{{ blog.title }}</h2>
+      <!-- remember v-if v-on, v-for? 
+      there will be a case when you want to do something inside your element, 
+      but VueJS doesn't provide you with that. you can just create a custom directives then.
+      in this case we want to make the blog list have title that colored just like rainbow.  -->
+      <h2 v-rainbow>{{ blog.title }}</h2>
       <article>{{ blog.body }}</article>
     </div>
     <!-- <add-blog></add-blog> -->
--- learn_vue_cli/vue-with-cli/src/main.js
+++ learn_vue_cli/vue-with-cli/src/main.js
@@ -6,6 +6,20 @@ import App from "./App.vue";
 //TADAA we successfully created a new EventBus, let's export it so it can be used everywhere under App.Vue
 export const bus = new Vue();
 
+//global custom directive (we can also make a local custom directive though)
+// the custom directive name is 'rainbow', that why we call 'v-rainbow' the 'v-' is just a vueJs rule to register it as a Vue things (object)
+Vue.directive("rainbow", {
+  bind(el, binding, node) {
+    // el : element itself
+    // binding : refers to the information about the connection if we passed in a value. example : v-rainbow="-this is the information-"
+    el.style.color =
+      "#" +
+      Math.random()
+        .toString()
+        .slice(2, 8);
+  }
+});
+
 // Vue.component("ninjas", Ninjas); // register the component at the main app
 new Vue({
   el: "#app",
