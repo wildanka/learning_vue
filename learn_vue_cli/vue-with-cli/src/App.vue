b1a838377a3bd323840d2d1b98e73fd13788595b
Wildan Kurniadi <wildan.kurniadi@klikdokter.com>
2021-02-20 12:39:18 +0700

    80 mixin : problems (say for example we have same behavior that using computed properties, we better use mixins)

--- learn_vue_cli/vue-with-cli/src/App.vue
+++ learn_vue_cli/vue-with-cli/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <show-blog></show-blog>
+    <list-blog></list-blog>
   </div>
 </template>
 
 <script>
 import addBlog from "./components/addBlog.vue";
 import showBlog from "./components/showBlogs.vue";
+import listBlog from "./components/listBlogs.vue";
 
 export default {
   components: {
     // add components
     "add-blog": addBlog,
     "show-blog": showBlog,
+    "list-blog": listBlog,
   },
   data() {
     return {};
---
+++ learn_vue_cli/vue-with-cli/src/components/listBlogs.vue
@@ -0,0 +1,78 @@
+<template>
+  <div v-theme:column="" id="show-blogs">
+    <h1>List Blog Titles</h1>
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <div v-for="blog in filteredBlogs" class="single-blog" v-bind:key="blog.id">
+      <h2 v-rainbow>{{ blog.title | toUppercase }}</h2>
+      <!-- <article>{{ blog.body | readMore }}</article> -->
+    </div>
+    <!-- <add-blog></add-blog> -->
+  </div>
+</template>
+
+<script>
+// Imports
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+export default {
+  data() {
+    return {
+      blogs: [],
+      search: "",
+    };
+  },
+  methods: {},
+  created() {
+    axios
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then((result) => {
+        //result.data
+        console.log(result.data.slice(10));
+        // this.blogs = result.data.slice(10);
+        this.blogs = result.data.slice(0, 10);
+      })
+      .catch((err) => {});
+  },
+  computed: {
+    filteredBlogs: function () {
+      return this.blogs.filter((blog) => {
+        return blog.title.match(this.search);
+      });
+    },
+  },
+  filters: {
+    //move toUppercase filters to local components
+    toUppercase: function (value) {
+      return value.toUpperCase();
+    },
+    //can also be written as
+    // toUppercase(value) {
+    //   return value.toUpperCase();
+    // },
+  },
+  directives: {
+    rainbow: {
+      bind(el, binding, node) {
+        // el : element itself
+        // binding : refers to the information about the connection if we passed in a value. example : v-rainbow="-this is the information-"
+        el.style.color = "#" + Math.random().toString().slice(2, 8);
+      },
+    },
+  },
+};
+</script>
+<style lang="">
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: lightblue;
+}
+</style>
