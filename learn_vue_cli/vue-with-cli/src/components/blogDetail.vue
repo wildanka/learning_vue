6d82e8f93963aac70e462ef7f165f8b9883ddccf
Wildan Kurniadi <wildan.kurniadi@klikdokter.com>
2021-02-21 22:33:33 +0700

    89 Route Parameters : Create Detail Blog Page

---
+++ learn_vue_cli/vue-with-cli/src/components/blogDetail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="blog-detail">
+    <h1>{{ blog.title }}</h1>
+    <article>{{ blog.body }}</article>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+export default {
+  data() {
+    return {
+      id: this.$route.params.blogId,
+      blog: {},
+    };
+  },
+  created() {
+    axios
+      .get("http://jsonplaceholder.typicode.com/posts/" + this.id)
+      .then((result) => {
+        //result.data
+        console.log(result.data);
+        // this.blogs = result.data.slice(10);
+        // this.blogs = result.data.slice(0, 10);
+        this.blog = result.data;
+      })
+      .catch((err) => {});
+  },
+};
+</script>
+<style >
+</style>
--- learn_vue_cli/vue-with-cli/src/routes.js
+++ learn_vue_cli/vue-with-cli/src/routes.js
@@ -1,7 +1,9 @@
 import showBlogs from './components/showBlogs.vue';
 import addBlog from './components/addBlog.vue';
+import blogDetail from './components/blogDetail.vue';
 
 export default [
     {path: '/', component: showBlogs},
     {path: '/add', component: addBlog},
+    {path: '/blog/:blogId', component: blogDetail},
 ]
